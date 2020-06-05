dc07784f92495640ca2979dd99b0e725df94ddef
Adenylatcyclase <streicher1993@googlemail.com>
2020-06-05 20:38:02 +0200

    change to types + tests

--- src/hash/sha2.rs
+++ src/hash/sha2.rs
@@ -1,53 +1,48 @@
-use std::iter::FromIterator;
-use std::fmt;
+#![allow(non_snake_case)]
 
+use std::iter::FromIterator;
+
+
+enum ShaType {
+    Sha256,
+    Sha224
+
+}
+impl ShaType {
+    pub fn get_h(&self) -> [u32; 8] {
+        match self {
+            Self::Sha256 => [0x6a09e667,  0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19],
+            Self::Sha224 => [0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4],
+        }
+    }
+
+    pub fn get_k (&self) -> Vec<u32> {
+        match self {
+            _ => vec![0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+                      0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+                      0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+                      0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+                      0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+                      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+                      0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+                      0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2],
+        }
+    }
+}
 pub struct Sha2 {
-    h0: u32,
-    h1: u32,
-    h2: u32,
-    h3: u32,
-    h4: u32,
-    h5: u32,
-    h6: u32,
-    h7: u32,
-    k: Vec<u32>
+    msg: Vec<u8>,
+    sha_type: ShaType,
 }
 
 impl Sha2 {
     pub fn sha256() -> Self {
-        Self{h0: 0x6a09e667,
-            h1: 0xbb67ae85,
-            h2: 0x3c6ef372,
-            h3: 0xa54ff53a,
-            h4: 0x510e527f,
-            h5: 0x9b05688c,
-            h6: 0x1f83d9ab,
-            h7: 0x5be0cd19,
-            k: Self::get_k()}
+        Self{msg: Vec::new(), sha_type: ShaType::Sha256}
     }
 
     pub fn sha224() -> Self {
-        Self{h0: 0xc1059ed8,
-            h1: 0x367cd507,
-            h2: 0x3070dd17,
-            h3: 0xf70e5939,
-            h4: 0xffc00b31,
-            h5: 0x68581511,
-            h6: 0x64f98fa7,
-            h7: 0xbefa4fa4,
-            k: Self::get_k()}
+        Self{msg: Vec::new(), sha_type: ShaType::Sha224}
     }
 
-    fn get_k () -> Vec<u32> {
-        vec![0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-             0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-             0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-             0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-             0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-             0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-             0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-             0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
-    }
     fn ch (x: u32, y: u32, z:u32) -> u32 {
         (x & y) ^ ((!x) & z)
     }
@@ -72,13 +67,13 @@ impl Sha2 {
         x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10)
     }
 
-    fn padding(message: String) -> Vec<u32> {
-        let mut bytes = message.into_bytes();
+    fn padding(&self) -> Vec<u32> {
+        let mut bytes = self.msg.clone();
         let n = (440 - (bytes.len() * 8) % 440) / 8;
         let l = (bytes.len() * 8) as u64;
         bytes.push(0x80);
 
-        for i in 0..n {
+        for _ in 0..n {
             bytes.push(0);
         }
         
@@ -94,8 +89,16 @@ impl Sha2 {
     }
 
     pub fn update(&mut self, message: String) {
+        self.msg.append(&mut message.into_bytes());
+    }
+
+    pub fn digest(&self) -> Vec<u8> {
+
         let mut w = vec![0; 64];
-        let message = Self::padding(message);
+        let message = self.padding();
+
+        let mut h0 = self.sha_type.get_h();
+        let k = self.sha_type.get_k();
 
         for chunk in message.chunks(16) {
 
@@ -112,19 +115,19 @@ impl Sha2 {
             }
 
             // Initialize working variables to current hash value:
-            let mut a = self.h0;
-            let mut b = self.h1;
-            let mut c = self.h2;
-            let mut d = self.h3;
-            let mut e = self.h4;
-            let mut f = self.h5;
-            let mut g = self.h6;
-            let mut h = self.h7;
+            let mut a = h0[0];
+            let mut b = h0[1];
+            let mut c = h0[2];
+            let mut d = h0[3];
+            let mut e = h0[4];
+            let mut f = h0[5];
+            let mut g = h0[6];
+            let mut h = h0[7];
 
             for i in 0..64 {
                 let S1 = Self::bsig1(e);
                 let ch = Self::ch(e, f, g);
-                let temp1 = h.overflowing_add(S1).0.overflowing_add(ch).0.overflowing_add(self.k[i]).0.overflowing_add(w[i]).0;
+                let temp1 = h.overflowing_add(S1).0.overflowing_add(ch).0.overflowing_add(k[i]).0.overflowing_add(w[i]).0;
                 let S0 = Self::bsig0(a);
                 let maj = Self::maj(a, b, c);
                 let temp2 = S0.overflowing_add(maj).0;
@@ -139,134 +142,88 @@ impl Sha2 {
                 a = temp1.overflowing_add(temp2).0;
             }
 
-            self.h0 = self.h0.overflowing_add(a).0;
-            self.h1 = self.h1.overflowing_add(b).0;
-            self.h2 = self.h2.overflowing_add(c).0;
-            self.h3 = self.h3.overflowing_add(d).0;
-            self.h4 = self.h4.overflowing_add(e).0;
-            self.h5 = self.h5.overflowing_add(f).0;
-            self.h6 = self.h6.overflowing_add(g).0;
-            self.h7 = self.h7.overflowing_add(h).0;
+            h0[0] = h0[0].overflowing_add(a).0;
+            h0[1] = h0[1].overflowing_add(b).0;
+            h0[2] = h0[2].overflowing_add(c).0;
+            h0[3] = h0[3].overflowing_add(d).0;
+            h0[4] = h0[4].overflowing_add(e).0;
+            h0[5] = h0[5].overflowing_add(f).0;
+            h0[6] = h0[6].overflowing_add(g).0;
+            h0[7] = h0[7].overflowing_add(h).0;
         }
-    }
 
-    pub fn digest(&self) -> Vec<u8> {
         let mut vec = Vec::new();
-        vec.append(&mut Vec::from_iter(self.h0.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h1.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h2.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h3.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h4.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h5.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h6.to_be_bytes().iter().cloned()));
-        vec.append(&mut Vec::from_iter(self.h7.to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[0].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[1].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[2].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[3].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[4].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[5].to_be_bytes().iter().cloned()));
+        vec.append(&mut Vec::from_iter(h0[6].to_be_bytes().iter().cloned()));
+        match self.sha_type {
+            ShaType::Sha256 => vec.append(&mut Vec::from_iter(h0[7].to_be_bytes().iter().cloned())),
+            _ => (),
+        }
+        
         vec
     }
 
     pub fn hexdigest(&self) -> String {
-        format!("{:X}{:X}{:X}{:X}{:X}{:X}{:X}{:X}", self.h0, self.h1, self.h2, self.h3, self.h4, self.h5, self.h6, self.h7)
+        let mut s = String::new();
+        for h in self.digest() {
+            s.push_str(&format!("{:02X}", h));
+        }
+        s
     }
+
 }
 
-pub fn sha2 (msg : String) -> String {
-    // Note 1: All variables are 32 bit unsigned integers and addition is calculated modulo 232
-    // Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i], 0 ≤ i ≤ 63
-    // Note 3: The compression function uses 8 working variables, a through h
-    // Note 4: Big-endian convention is used when expressing the constants in this pseudocode,
-    //     and when parsing message block data from bytes to words, for example,
-    //     the first word of the input message "abc" after padding is 0x61626380
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn sha256_test() {
+        // Test single use
+        let mut sha = Sha2::sha256();
+        sha.update("Hello, World!".to_string());
+        assert_eq!(sha.hexdigest(), "DFFD6021BB2BD5B0AF676290809EC3A53191DD81C7F70A4B28688A362182986F".to_string());
 
-    // Initialize hash values:
-    // (first 32 bits of the fractional parts of the square roots of the first 8 primes 2..19):
-    let mut h0 : u32 = 0x6a09e667;
-    let mut h1 : u32 = 0xbb67ae85;
-    let mut h2 : u32 = 0x3c6ef372;
-    let mut h3 : u32 = 0xa54ff53a;
-    let mut h4 : u32 = 0x510e527f;
-    let mut h5 : u32 = 0x9b05688c;
-    let mut h6 : u32 = 0x1f83d9ab;
-    let mut h7 : u32 = 0x5be0cd19;
+        assert_eq!(sha.digest(), [0xDF, 0xFD, 0x60, 0x21, 0xBB, 0x2B, 0xD5, 0xB0, 
+                                0xAF, 0x67, 0x62, 0x90, 0x80, 0x9E, 0xC3, 0xA5, 
+                                0x31, 0x91, 0xDD, 0x81, 0xC7, 0xF7, 0x0A, 0x4B, 
+                                0x28, 0x68, 0x8A, 0x36, 0x21, 0x82, 0x98, 0x6F]);
 
-    let mut v = msg.into_bytes();
-    
-    // Initialize array of round constants:
-    // (first 32 bits of the fractional parts of the cube roots of the first 64 primes 2..311):
-    let k : Vec<u32> = vec![0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-                            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-                            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-                            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-                            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-                            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-                            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-                            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2];
-    
-    // Pre-processing (Padding)
-    let rest = 512 - (v.len() * 8 + 1 + 64) % 512;
-    let l = (v.len() * 8) as u64;
-    v.push(0x80);
+        // Test multiple use like Pythons hashlib
+        let mut sha1 = Sha2::sha256();
+        sha1.update("Bananensaft".to_string());
+        assert_eq!(sha1.hexdigest(), "23814372675176D5115189E78BEA52E992E09F47CD0CF2E3D446F4FA4C9568FA".to_string());
+        sha1.update("Schokokuchen".to_string());
 
-    for i in 0..(rest - 7)/8 {
-        v.push(0);
-    }
-    
-    v.append(&mut Vec::from_iter(l.to_be_bytes().iter().cloned()));
-
-    let mut vv = Vec::new();
-    let mut buf = [0u8; 4];
-    for i in v.chunks(4) {
-        buf.clone_from_slice(i);
-        vv.push(u32::from_be_bytes(buf));
+        let mut sha2 = Sha2::sha256();
+        sha2.update("BananensaftSchokokuchen".to_string());
+        assert_eq!(sha1.hexdigest(), sha2.hexdigest()); 
     }
 
-    for v in vv.chunks(16) {
-        let mut w = vec![0; 64];
-        // (The initial values in w[0..63] don't matter, so many implementations zero them here)
-        for (e, u) in v.iter().enumerate() {
-            w[e] = *u;
-        }
-        // Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array:
-        for e in 16..64 {
-            let s0 = w[e-15].rotate_right(7) ^ w[e-15].rotate_right(18) ^ (w[e-15] >> 3);
-            let s1 = w[e- 2].rotate_right(17) ^ w[e- 2].rotate_right(19) ^ (w[e- 2] >> 10);
-            w[e] = w[e-16].overflowing_add(s0).0.overflowing_add(w[e-7]).0.overflowing_add(s1).0;
-        }
+    #[test]
+    fn sha224_test() {
+        // Test single use
+        let mut sha = Sha2::sha224();
+        sha.update("Hello, World!".to_string());
+        assert_eq!(sha.hexdigest(), "72A23DFA411BA6FDE01DBFABF3B00A709C93EBF273DC29E2D8B261FF".to_string());
 
-        // Initialize working variables to current hash value:
-        let mut a = h0;
-        let mut b = h1;
-        let mut c = h2;
-        let mut d = h3;
-        let mut e = h4;
-        let mut f = h5;
-        let mut g = h6;
-        let mut h = h7;
+        assert_eq!(sha.digest(), [0x72, 0xA2, 0x3D, 0xFA, 0x41, 0x1B, 0xA6,
+                                  0xFD, 0xE0, 0x1D, 0xBF, 0xAB, 0xF3, 0xB0,
+                                  0x0A, 0x70, 0x9C, 0x93, 0xEB, 0xF2, 0x73,
+                                  0xDC, 0x29, 0xE2, 0xD8, 0xB2, 0x61, 0xFF]);
 
-        for j in 0..64 {
-            let s1 = e.rotate_right(6) ^ e.rotate_right(11) ^ e.rotate_right(25);
-            let ch = (e & f) ^ ((!e) & g);
-            let temp1 = h.overflowing_add(s1).0.overflowing_add(ch).0.overflowing_add(k[j]).0.overflowing_add(w[j]).0;
-            let s0 = a.rotate_right(2) ^ a.rotate_right(13) ^ a.rotate_right(22);
-            let maj = (a & b) ^ (a & c) ^ (b & c);
-            let temp2 = s0.overflowing_add(maj).0;
+        // Test multiple use like Pythons hashlib
+        let mut sha1 = Sha2::sha224();
+        sha1.update("Bananensaft".to_string());
+        assert_eq!(sha1.hexdigest(), "FA25E7A96CFAFF5AAB97F0CC4D6E0252DA1F39B3789AB096FAD39228".to_string());
+        sha1.update("Schokokuchen".to_string());
 
-            h = g;
-            g = f;
-            f = e;
-            e = d.overflowing_add(temp1).0;
-            d = c;
-            c = b;
-            b = a;
-            a = temp1.overflowing_add(temp2).0;
-        }
-
-        h0 = h0.overflowing_add(a).0;
-        h1 = h1.overflowing_add(b).0;
-        h2 = h2.overflowing_add(c).0;
-        h3 = h3.overflowing_add(d).0;
-        h4 = h4.overflowing_add(e).0;
-        h5 = h5.overflowing_add(f).0;
-        h6 = h6.overflowing_add(g).0;
-        h7 = h7.overflowing_add(h).0;
+        let mut sha2 = Sha2::sha224();
+        sha2.update("BananensaftSchokokuchen".to_string());
+        assert_eq!(sha1.hexdigest(), sha2.hexdigest()); 
     }
-    format!("{:X}{:X}{:X}{:X}{:X}{:X}{:X}{:X}", h0, h1, h2, h3, h4, h5, h6, h7)
 }
