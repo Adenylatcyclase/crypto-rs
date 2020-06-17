f0fdea4a1211f4ee1f91b2a2eeb107674f2c72ec
Adenylatcyclase <streicher1993@googlemail.com>
2020-06-17 22:16:59 +0200

    ToBytes trait

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,2 +1,3 @@
 pub mod crypto_math;
 pub mod padding;
+pub mod to_bytes;
--- src/misc/to_bytes.rs
+++ src/misc/to_bytes.rs
@@ -1,4 +1,33 @@
-pub trait toBytes{
-    fn to_be_bytes(&self) -> Vec<u8>;
-    fn to_le_bytes(&self) -> Vec<u8>;
+use std::iter::FromIterator;
+
+pub trait ToBytes{
+    fn to_bytes_be(&self) -> Vec<u8>;
+    fn to_bytes_le(&self) -> Vec<u8>;
+}
+
+impl ToBytes for String {
+    fn to_bytes_be(&self) -> Vec<u8>{
+        self.clone().into_bytes()
+    }
+    fn to_bytes_le(&self) -> Vec<u8>{
+        self.clone().into_bytes()
+    }
+}
+
+impl ToBytes for u64 {
+    fn to_bytes_be(&self) -> Vec<u8>{
+        Vec::from_iter(self.to_be_bytes().iter().cloned())
+    }
+    fn to_bytes_le(&self) -> Vec<u8>{
+        Vec::from_iter(self.to_le_bytes().iter().cloned())
+    }
+}
+
+impl ToBytes for u32 {
+    fn to_bytes_be(&self) -> Vec<u8>{
+        Vec::from_iter(self.to_be_bytes().iter().cloned())
+    }
+    fn to_bytes_le(&self) -> Vec<u8>{
+        Vec::from_iter(self.to_le_bytes().iter().cloned())
+    }
 }
