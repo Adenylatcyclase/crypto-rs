ba5c699f8bf291f72bc8060d5a167d7f681129f5
Adenylatcyclase <streicher1993@googlemail.com>
2020-06-18 14:14:45 +0200

    tests for cryptomath

--- src/misc/crypto_math.rs
+++ src/misc/crypto_math.rs
@@ -25,3 +25,20 @@ fn galois_pow(i: usize) -> u8{
     }
     rc as u8
 }
+
+
+#[test]
+    fn gal_mul_test() {
+        assert_eq!(galois_mul(0x53, 0xca), 1);
+        assert_eq!(galois_mul(0x57, 0x83), 193);
+    }
+
+    #[test]
+    fn gal_pow_test() {
+        assert_eq!(galois_pow(1), 1, "rcon 1 is wrong");
+        assert_eq!(galois_pow(4), 8, "rcon 4 is wrong");
+        assert_eq!(galois_pow(6), 0x20, "rcon 4 is wrong");
+        assert_eq!(galois_pow(8), 0x80, "rcon 8 is wrong");
+        assert_eq!(galois_pow(9), 0x1B, "rcon 9 is wrong");
+        assert_eq!(galois_pow(10), 0x36, "rcon 10 is wrong");
+    }
